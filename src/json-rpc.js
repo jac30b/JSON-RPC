deb91bd64b019d360e5533ffc8d8852d1f7b2d74
jac30b <galeckijakub9@gmail.com>
2021-10-03 13:17:53 +0200

    sx

--- src/json-rpc.js
+++ src/json-rpc.js
@@ -4,6 +4,7 @@ module.exports = class JSONRpc {
     resolve(data){
         try {
             const parsed = JSON.parse(data);
+            console.log(parsed);
             return this.parser.parse(parsed);
         } catch (e) {
             throw new Error("Invalid Request");
--- src/parser.js
+++ src/parser.js
@@ -3,21 +3,20 @@ const {Response} = require('./response');
 const {Notification} = require('./notification');
 
 module.exports = class Parser {
-    constructor(){}
     static parse(obj) {
         if (obj && typeof obj === 'string') {
             try {
                 const tmp = JSON.parse(obj);
-                switch (tmp) {
-                    case Request.isRequest(tmp):
+        
+                    if(Request.isRequest(tmp)){
                         Request.isValidRequest(tmp);
-                        return new Request(tmp);
-                    case Response.isResponse(tmp):
+                        return new Request({id, method, params, jsonrpc});
+                    } else if (Response.isResponse(tmp)) {
                         Response.isValidResponse(tmp);
-                        return new Response(tmp);
-                    case Notification.isNotification(tmp):
+                        return new Response()
+                    } else if ( Notification.isNotification(tmp)){
                         Notification.isValidNotification(tmp);
-                        return new Notification(tmp);
+                    }
                 }
             } catch (e) {
                 console.error(e);
